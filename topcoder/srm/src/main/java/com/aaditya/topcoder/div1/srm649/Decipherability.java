254222b11df2e6b9e898a645d3080922178e9a39
Aaditya Ramesh <aramesh@conviva.com>
2015-10-03 17:35:33 -0700

    [Topcoder] Fix issue with SRM 649.

--- topcoder/srm/src/main/java/com/aaditya/topcoder/div1/srm649/Decipherability.java
+++ topcoder/srm/src/main/java/com/aaditya/topcoder/div1/srm649/Decipherability.java
@@ -4,7 +4,7 @@ public class Decipherability {
     public static String check(String s, int K) {
         for(int i = 0; i < s.length(); i++) {
             for(int j = i; j < s.length(); j++) {
-                if(s.charAt(i) == s.charAt(j) && j - i >= K) {
+                if(s.charAt(i) == s.charAt(j) && j - i <= K) {
                     return "Uncertain";
                 }
             }
