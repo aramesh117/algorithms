43bbdfddc35f9e2a3eab71baaf22e270dc3d592f
Aaditya Ramesh <aramesh@conviva.com>
2015-10-24 19:04:25 -0700

    [SRM 649] 1000 point solution.

---
+++ topcoder/srm/src/main/java/com/aaditya/topcoder/div2/srm649/XorSequenceEasy.java
@@ -0,0 +1,28 @@
+package com.aaditya.topcoder.div2.srm649;
+
+public class XorSequenceEasy {
+    public static int getmax(int[] A, int N) {
+        int numBits = Integer.bitCount(N - 1);
+        int[][] largestBitCounts = new int[numBits][2];
+        // Count the number of pairs.
+        for (int j = 0; j < A.length; j++) {
+            for (int k = j + 1; k < A.length; k++) {
+                for (int i = numBits - 1; i >= 0; i--) {
+                    if (((A[j] >> i) & 1) != ((A[k] >> i) & 1)) {
+                        largestBitCounts[i][(A[j] >> i) & 1]++;
+                        break;
+                    }
+                }
+            }
+        }
+        int total = 0;
+        for (int i = 0; i < numBits; i++) {
+            total += Math.max(largestBitCounts[i][0], largestBitCounts[i][1]);
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getmax(new int[]{3, 2, 1, 0, 3, 2}, 4));
+    }
+}
