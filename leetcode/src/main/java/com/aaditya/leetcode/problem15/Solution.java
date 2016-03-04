3a54f09bf2914aeca85e7f178ba86e6328c1c2c8
Aaditya Ramesh <aramesh@conviva.com>
2016-03-04 12:36:35 -0800

    [Leetcode] Added solutions to 15,17,22.

--- leetcode/src/main/java/com/aaditya/leetcode/problem15/Solution.java
+++ leetcode/src/main/java/com/aaditya/leetcode/problem15/Solution.java
@@ -1,15 +1,59 @@
 package com.aaditya.leetcode.problem15;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     public static List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 3) {
+            return new ArrayList<>();
+        }
         Arrays.sort(nums);
+        if (nums.length == 3) {
+            if (nums[0] + nums[1] + nums[2] == 0) {
+                List<List<Integer>> list = new ArrayList<>();
+                List<Integer> list1 = new ArrayList<>();
+                list1.add(nums[0]);
+                list1.add(nums[1]);
+                list1.add(nums[2]);
+                list.add(list1);
+                return list;
+            }
+        }
 
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && (nums[i] == nums[i - 1]))
+                continue;
+            int a = nums[i];
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int b = nums[start];
+                int c = nums[end];
+                if (a + b + c == 0) {
+                    res.add(Arrays.asList(a, b, c));
+                    while (start < end && nums[start] == b) start++;
+                    while (start < end && nums[end] == c) end--;
+                } else {
+                    if (a + b + c > 0) {
+                        while (end > 0 && nums[end] == c) {
+                            end--;
+                        }
+                    } else {
+                        while (start < nums.length && nums[start] == b) {
+                            start++;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
     }
 
+
     public static void main(String[] args) {
-        System.out.println(threeSum(new int[]{7, -1, 14, -12, -8, 7, 2, -15, 8, 8, -8, -14, -4, -5, 7, 9, 11, -4, -15, -6, 1, -14, 4, 3, 10, -5, 2, 1, 6, 11, 2, -2, -5, -7, -6, 2, -15, 11, -6, 8, -4, 2, 1, -1, 4, -6, -15, 1, 5, -15, 10, 14, 9, -8, -6, 4, -6, 11, 12, -15, 7, -1, -9, 9, -1, 0, -4, -1, -12, -2, 14, -9, 7, 0, -3, -4, 1, -2, 12, 14, -10, 0, 5, 14, -1, 14, 3, 8, 10, -8, 8, -5, -2, 6, -11, 12, 13, -7, -12, 8, 6, -13, 14, -2, -5, -11, 1, 3, -6}));
+        System.out.println(threeSum(new int[]{-2,-3,0,0,-2}));
     }
 }
